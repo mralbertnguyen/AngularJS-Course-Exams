ea3d6a2f0105d9a1485a7fa4356c3b7a739f26f0
Albert Nguyen <ndttam.dev@gmail.com>
2019-11-28 21:46:56 +0700

    know how to use controller to change data in model

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 var app = angular.module('expensesApp',[]);
 
 app.controller('ExpensesViewController',['$scope', function($scope){
-    $scope.name  = 'Albert';
+    $scope.userModel  = {
+        name : 'Albert',
+        old : 21
+    };
+
+    $scope.title = 'Angular Exam';
+
+    $scope.changeName = function(){
+        if($scope.userModel.name == 'Albert'){
+            $scope.userModel.name = 'Tam'
+        }else{
+            $scope.userModel.name = 'Albert'
+        }
+    }
+
+    $scope.increaseOld = function(){
+        $scope.userModel.old++;
+    }
+
+    $scope.decreaseOld = function(){
+        $scope.userModel.old--;
+    }
 }])
